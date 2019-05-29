05b3230bb4f0102570c7ac938469941abfb49276
VB-Cloudboy <Vijayb@cloudboy.in>
2019-05-29 10:38:59 +0530

    updates in the S3 File Movement

---
+++ aws_s3/s3_fileMovement.py
@@ -0,0 +1,12 @@
+import boto3
+
+s3 = boto3.resource('s3', aws_access_key_id = 'MyAccessKey', aws_secret_access_key = 'MySecretAccessKey')
+
+dataPath=(r'C:\Newfolder\*.zip');
+myBucket='brelbbucket';
+fileName=['localdata.zip','Myself.zip']
+
+for file in fileName:
+	s3.meta.client.upload_file(dataPath, myBucket, file)
+else:
+	print("No file available for movement")
